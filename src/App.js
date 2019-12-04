740e7b3461b86fd28a8488d48001000af9b24c93
Andrey Gritsuk Viktorovich <axa8xa8@gmail.com>
2019-12-04 14:34:46 +0300

    Start template React without modules

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <img
+          src="https://image.flaticon.com/icons/svg/783/783347.svg"
+          alt="global logo"
+        />
       </header>
+
+      <nav className="nav">
+        <div>
+          <a href="#q1">Profile</a>
+        </div>
+        <div>
+          <a href="#q2">Messages</a>
+        </div>
+        <div>
+          <a href="#q3">News</a>
+        </div>
+        <div>
+          <a href="#q4">Music</a>
+        </div>
+        <div>
+          <a href="#q5">Settings</a>
+        </div>
+      </nav>
+
+      <div className="content">
+        <img
+          src="https://foter.com/photos/394/yellow-taxi-on-city-street.jpg"
+          alt="account wallpaper"
+        />
+        <div>
+          <div> Ava +description</div>
+          <div>
+            My posts
+            <div>New post</div>
+            <div>
+              <div>post 1</div>
+              <div>post 2</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
